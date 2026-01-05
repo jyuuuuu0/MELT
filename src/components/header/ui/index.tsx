44238a7ffd678f21f1d1888308fe13a3b852554b
jyuuuuu0 <s25023@gsm.hs.kr>
2026-01-05 12:02:28 +0900

    design: 헤더 크기 수정

--- src/components/header/ui/index.tsx
+++ src/components/header/ui/index.tsx
@@ -9,7 +9,7 @@ import { usePathname } from 'next/navigation';
 export default function Header() {
   const pathname = usePathname();
   return (
-    <header className="flex items-center justify-between px-[600px] py-5 bg-white border-b border-[#E3E3E3]">
+    <header className="flex items-center justify-between w-full h-[70px] px-[600px] py-[15px] bg-white border-b border-[#E3E3E3]">
       <Link href="/today">
         <Image src={logo} width={63} height={30} alt="logo" />
       </Link>
@@ -21,7 +21,9 @@ export default function Header() {
               key={item.path}
               href={item.path}
               className={`px-3 py-[10px] rounded-xl ${
-                isActive ? 'bg-[#009691] text-white font-semibold' : 'text-black font-medium'
+                isActive
+                  ? ' flex items-center justify-center bg-[#009691] w-[52px] h-[39px] text-white font-semibold'
+                  : 'text-black font-medium'
               }`}
             >
               {item.label}
