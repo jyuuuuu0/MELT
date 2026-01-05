0f4cef5484a969dfd3640308fa8c7f1be544546b
jyuuuuu0 <s25023@gsm.hs.kr>
2026-01-05 11:57:53 +0900

    feat: header 컴포넌트

---
+++ src/components/header/ui/index.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '@/asset/img/logo.png';
+import { HEADER_NAV } from '../config';
+import { usePathname } from 'next/navigation';
+
+export default function Header() {
+  const pathname = usePathname();
+  return (
+    <header className="flex items-center justify-between px-[600px] py-5 bg-white border-b border-[#E3E3E3]">
+      <Link href="/today">
+        <Image src={logo} width={63} height={30} alt="logo" />
+      </Link>
+      <div className="flex items-center gap-[30px] font-medium">
+        {HEADER_NAV.map((item) => {
+          const isActive = pathname === item.path;
+          return (
+            <Link
+              key={item.path}
+              href={item.path}
+              className={`px-3 py-[10px] rounded-xl ${
+                isActive ? 'bg-[#009691] text-white font-semibold' : 'text-black font-medium'
+              }`}
+            >
+              {item.label}
+            </Link>
+          );
+        })}
+      </div>
+    </header>
+  );
+}
