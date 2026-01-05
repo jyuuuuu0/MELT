7d525ce836c79b0e0c549513b590327f282c994c
jyuuuuu0 <s25023@gsm.hs.kr>
2026-01-05 22:41:41 +0900

    feat: 오늘의 급식

---
+++ src/components/today/today.tsx
@@ -0,0 +1,34 @@
+export default function TodayBox() {
+  return (
+    <div className="flex flex-col items-start justify-center w-[397px] h-[333px] px-[31px] py-[40px] bg-white border border-[#9C9C9C] rounded-2xl gap-5">
+      <div className="flex items-center gap-5">
+        <div className="w-2 h-2 rounded-full bg-[#009691]" />
+        <p>친환경쌀밥</p>
+      </div>
+      <div className="flex items-center gap-5">
+        <div className="w-2 h-2 rounded-full bg-[#009691]" />
+        <p>열무된장국</p>
+      </div>
+      <div className="flex items-center gap-5">
+        <div className="w-2 h-2 rounded-full bg-[#009691]" />
+        <p>시금치유부무침</p>
+      </div>
+      <div className="flex items-center gap-5">
+        <div className="w-2 h-2 rounded-full bg-[#009691]" />
+        <p>돼지갈비구이</p>
+      </div>
+      <div className="flex items-center gap-5">
+        <div className="w-2 h-2 rounded-full bg-[#009691]" />
+        <p>군고구마</p>
+      </div>
+      <div className="flex items-center gap-5">
+        <div className="w-2 h-2 rounded-full bg-[#009691]" />
+        <p>깍두기</p>
+      </div>
+      <div className="flex items-center gap-5">
+        <div className="w-2 h-2 rounded-full bg-[#009691]" />
+        <p>흰우유</p>
+      </div>
+    </div>
+  );
+}
