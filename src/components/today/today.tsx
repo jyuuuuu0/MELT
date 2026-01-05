ec1955fc524357fda3ef5329f6654242b865063b
jyuuuuu0 <s25023@gsm.hs.kr>
2026-01-05 23:01:49 +0900

    feat: 반응형 설정

--- src/components/today/today.tsx
+++ src/components/today/today.tsx
@@ -1,33 +1,37 @@
 export default function TodayBox() {
   return (
-    <div className="flex flex-col items-start justify-center w-[397px] h-[333px] px-[31px] py-[40px] bg-white border border-[#9C9C9C] rounded-2xl gap-5">
+    <div
+      className="flex flex-col items-start justify-center w-full max-w-[397px]
+ pl-5 pr-40 py-6 lg:pl-[31px] sm:pr-10 lg:pr-[220px] lg:py-[40px]
+ bg-white border border-[#9C9C9C] rounded-2xl gap-4 lg:gap-5"
+    >
       <div className="flex items-center gap-5">
         <div className="w-2 h-2 rounded-full bg-[#009691]" />
-        <p>친환경쌀밥</p>
+        <p className="text-sm md:text-base">친환경쌀밥</p>
       </div>
       <div className="flex items-center gap-5">
         <div className="w-2 h-2 rounded-full bg-[#009691]" />
-        <p>열무된장국</p>
+        <p className="text-sm md:text-base">열무된장국</p>
       </div>
       <div className="flex items-center gap-5">
         <div className="w-2 h-2 rounded-full bg-[#009691]" />
-        <p>시금치유부무침</p>
+        <p className="text-sm md:text-base">시금치유부무침</p>
       </div>
       <div className="flex items-center gap-5">
         <div className="w-2 h-2 rounded-full bg-[#009691]" />
-        <p>돼지갈비구이</p>
+        <p className="text-sm md:text-base">돼지갈비구이</p>
       </div>
       <div className="flex items-center gap-5">
         <div className="w-2 h-2 rounded-full bg-[#009691]" />
-        <p>군고구마</p>
+        <p className="text-sm md:text-base">군고구마</p>
       </div>
       <div className="flex items-center gap-5">
         <div className="w-2 h-2 rounded-full bg-[#009691]" />
-        <p>깍두기</p>
+        <p className="text-sm md:text-base">깍두기</p>
       </div>
       <div className="flex items-center gap-5">
         <div className="w-2 h-2 rounded-full bg-[#009691]" />
-        <p>흰우유</p>
+        <p className="text-sm md:text-base">흰우유</p>
       </div>
     </div>
   );
--- src/view/today/index.tsx
+++ src/view/today/index.tsx
@@ -6,11 +6,11 @@ export default function TodayView() {
     <div className="w-full min-h-screen bg-[#009691]/5 flex flex-col">
       <Header />
       <div className="flex flex-1 flex-col items-center justify-center">
-        <div className="flex flex-col gap-[30px]">
+        <div className="flex flex-col gap-6 md:gap-[30px]">
           <div className="flex flex-col gap-1">
-            <div className="flex items-end gap-[7px] items-center">
-              <p className="font-semibold text-[40px]">점심</p>
-              <p className="text-sm text-[#9C9C9C] mb-2">745.8kcal</p>
+            <div className="flex items-end gap-2 md:gap-[7px]">
+              <p className="font-semibold  text-[28px] md:text-[32px] lg:text-[40px]">점심</p>
+              <p className="text-xs md:text-sm text-[#9C9C9C] mb-2">745.8kcal</p>
             </div>
             <p className="text-[#9C9C9C]">2026년 1월 1일 목요일</p>
           </div>
