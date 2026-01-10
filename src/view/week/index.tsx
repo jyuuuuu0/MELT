d71976eb81fe14275ffc462ec05ba49894f575d2
jyuuuuu0 <s25023@gsm.hs.kr>
2026-01-10 17:32:43 +0900

    fix: 정렬 수정

--- src/view/week/index.tsx
+++ src/view/week/index.tsx
@@ -6,7 +6,7 @@ export default function WeekView() {
   return (
     <div className="w-full min-h-screen bg-[#009691]/5 flex flex-col">
       <Header />
-      <div className="min-h-[calc(100vh-70px)] flex flex-col items-start justify-center pt-[153px] pb-[327px] px-[292px]">
+      <div className="min-h-[calc(100vh-70px)] flex flex-col pt-24 pb-32 px-4 sm:px-8 lg:px-24 xl:px-[292px] xl:py-[153px]">
         <div className="flex flex-col gap-[30px] mb-[60px]">
           <p className="font-semibold text-[40px]">주간 급식</p>
           <TimeButton />
